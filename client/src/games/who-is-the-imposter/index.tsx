785979fa3b0872262fd022864415ce67336513d1
GabrielBrotas <gabrielbrotas22@gmail.com>
2024-05-02 16:51:29 -0300

    env

--- client/src/games/who-is-the-imposter/index.tsx
+++ client/src/games/who-is-the-imposter/index.tsx
@@ -35,7 +35,7 @@ export function WhoIsTheImposter() {
   const [ws, setWs] = useState<WebSocket | null>(null);
 
   useEffect(() => {
-    const newWs = new WebSocket("ws://localhost:8081/ws");
+    const newWs = new WebSocket(`ws://${process.env.NEXT_PUBLIC_API_URL}/ws`);
     setWs(newWs);
 
     newWs.onopen = () => handleWebSocketOpen(newWs);
