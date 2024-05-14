e86b8e2331b0f916aa60ac9882efd9adac0cfe7e
GabrielBrotas <gabrielbrotas22@gmail.com>
2024-05-14 09:30:57 -0300

    more questions and visibility change event

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",  // <=== enables static exports
-  // reactStrictMode: true,
-  // basePath: "/who-is-the-imposter",
 };
 
 export default nextConfig;
--- server/cmd/spyfall_handlers.go
+++ server/cmd/spyfall_handlers.go
@@ -22,6 +22,7 @@ var (
 )
 
 func handleGetSpyfallPlayerList(w http.ResponseWriter, r *http.Request) {
+	log.Println("[handleGetSpyfallPlayerList] Getting Spyfall player list")
 	players := spyfallManager.GetPlayerList(true)
 	playerList := make([]spyfall.PlayerOut, 0, len(players))
 
@@ -29,13 +30,15 @@ func handleGetSpyfallPlayerList(w http.ResponseWriter, r *http.Request) {
 		playerList = append(playerList, *player.ToOut())
 	}
 
+	log.Printf("[handleGetSpyfallPlayerList] Returning player list: %v", playerList)
 	respondWithJSON(w, http.StatusOK, map[string]interface{}{"players": playerList})
 }
 
 func handleSpyfallConnections(w http.ResponseWriter, r *http.Request) {
+	log.Println("[handleSpyfallConnections] Handling new Spyfall connection")
 	conn, err := spyfallUpgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Printf("Error upgrading WebSocket connection: %v", err)
+		log.Printf("[handleSpyfallConnections] Error upgrading WebSocket connection: %v", err)
 		return
 	}
 	defer conn.Close()
@@ -45,44 +48,47 @@ func handleSpyfallConnections(w http.ResponseWriter, r *http.Request) {
 }
 
 func spyfallMessageLoop(conn *websocket.Conn) {
-	log.Printf("New Spyfall connection: %v", conn.RemoteAddr())
+	log.Printf("[spyfallMessageLoop] New Spyfall connection: %v", conn.RemoteAddr())
 	for {
 		msg := make(map[string]interface{})
 		if err := conn.ReadJSON(&msg); err != nil {
-			log.Printf("Error reading JSON from Spyfall client %v: %v", conn.RemoteAddr(), err)
+			log.Printf("[spyfallMessageLoop] Error reading JSON from Spyfall client %v: %v", conn.RemoteAddr(), err)
 			break
 		}
-		log.Printf("Received message at Spyfall: %v", msg)
 		if err := processSpyfallMessage(conn, msg); err != nil {
-			log.Printf("Error processing Spyfall message from %v: %v", conn.RemoteAddr(), err)
+			log.Printf("[spyfallMessageLoop] Error processing Spyfall message from %v: %v", conn.RemoteAddr(), err)
 			break
 		}
 	}
 }
 
 func handleCloseSpyfallConnection(conn *websocket.Conn) {
-	log.Printf("Closing Spyfall connection: %v", conn.RemoteAddr())
+	log.Printf("[handleCloseSpyfallConnection] Closing Spyfall connection: %v", conn.RemoteAddr())
 	spyfallManager.RemoveConnection(conn)
 	spyfallManager.BroadcastPlayerList()
 }
 
 func processSpyfallMessage(conn *websocket.Conn, msg map[string]interface{}) error {
+	log.Printf("[processSpyfallMessage] Processing message: %v", msg)
 	switch msg["type"].(string) {
 	case "connected":
 		return handleSpyfallConnected(conn, msg)
 	case "startGame":
 		return handleSpyfallStartGame(msg)
 	case "resetGame":
+		log.Println("[processSpyfallMessage] Resetting game")
 		spyfallManager.ResetGame()
 	case "removePlayer":
 		return handleSpyfallRemovePlayer(msg) // v
 	case "decideWinner":
 		return handleSpyfallDecideWinner(msg)
 	case "resetPoints":
+		log.Println("[processSpyfallMessage] Resetting points")
 		spyfallManager.ResetPoints()
 		spyfallManager.BroadcastPlayerList()
 		return nil
 	case "showSpiesNumber":
+		log.Println("[processSpyfallMessage] Showing spies number")
 		spyfallManager.BroadcastSpiesNumber()
 		return nil
 	default:
@@ -92,18 +98,22 @@ func processSpyfallMessage(conn *websocket.Conn, msg map[string]interface{}) err
 }
 
 func handleSpyfallConnected(conn *websocket.Conn, msg map[string]interface{}) error {
+	log.Printf("[handleSpyfallConnected] Handling connected message: %v", msg)
 	userID, err := parseUUID(msg, "id")
 	if err != nil {
+		log.Printf("[handleSpyfallConnected] Error parsing user ID: %v", err)
 		return err
 	}
 
 	user, err := usersRepository.GetUser(userID)
 	if err != nil {
+		log.Printf("[handleSpyfallConnected] Error getting user: %v", err)
 		return err
 	}
 
 	err = spyfallManager.RegisterPlayer(conn, user)
 	if err != nil {
+		log.Printf("[handleSpyfallConnected] Error registering player: %v", err)
 		return err
 	}
 
@@ -113,6 +123,7 @@ func handleSpyfallConnected(conn *websocket.Conn, msg map[string]interface{}) er
 
 // handleStartGame initializes a new game with specified settings.
 func handleSpyfallStartGame(msg map[string]interface{}) error {
+	log.Println("[handleSpyfallStartGame] Starting game")
 	dist := parseSpyfallDistribution(msg)
 	spyfallManager.StartGame(dist)
 	return nil
@@ -120,8 +131,10 @@ func handleSpyfallStartGame(msg map[string]interface{}) error {
 
 // handleSpyfallRemovePlayer removes a player from the game.
 func handleSpyfallRemovePlayer(msg map[string]interface{}) error {
+	log.Println("[handleSpyfallRemovePlayer] Removing player")
 	userID, err := parseUUID(msg, "id")
 	if err != nil {
+		log.Printf("[handleSpyfallRemovePlayer] Error parsing user ID: %v", err)
 		return err
 	}
 	spyfallManager.RemovePlayerByID(userID)
@@ -131,6 +144,7 @@ func handleSpyfallRemovePlayer(msg map[string]interface{}) error {
 
 // handleSpyfallDecideWinner updates the game state based on the winner
 func handleSpyfallDecideWinner(msg map[string]interface{}) error {
+	log.Println("[handleSpyfallDecideWinner] Deciding winner")
 	spyWon, _ := msg["spyWon"].(bool)
 	spyfallManager.FinishGame(spyWon)
 	spyfallManager.BroadcastPlayerList()
@@ -139,10 +153,12 @@ func handleSpyfallDecideWinner(msg map[string]interface{}) error {
 
 // handleSpyfallGetPlayerGameStatus returns the current game status for a user.
 func handleSpyfallGetPlayerGameStatus(w http.ResponseWriter, r *http.Request) {
+	log.Println("[handleSpyfallGetPlayerGameStatus] Getting player game status")
 	id := r.URL.Query().Get("u")
 	userID, err := uuid.Parse(id)
 
 	if err != nil {
+		log.Printf("[handleSpyfallGetPlayerGameStatus] Error parsing user ID: %v", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -150,6 +166,7 @@ func handleSpyfallGetPlayerGameStatus(w http.ResponseWriter, r *http.Request) {
 	user, err := usersRepository.GetUser(userID)
 
 	if err != nil {
+		log.Printf("[handleSpyfallGetPlayerGameStatus] Error getting user: %v", err)
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
@@ -160,6 +177,7 @@ func handleSpyfallGetPlayerGameStatus(w http.ResponseWriter, r *http.Request) {
 }
 
 func parseSpyfallDistribution(msg map[string]interface{}) spyfall.SpiesDistribution {
+	log.Printf("[parseSpyfallDistribution] Parsing spies distribution: %v", msg)
 	return spyfall.SpiesDistribution{
 		One:   int(msg["one"].(float64)),
 		Two:   int(msg["two"].(float64)),
